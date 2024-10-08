7d976c48fc19752a50a63be54b9785ed105d636a
Denu8 <kingdenuwan07@gmail.com>
2024-10-08 12:52:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "zOIUFSDZ#Ex5bQnZHzkhJFRLBoR13B7DASpVYGSxgYJ4Jx5-kwIs",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
+SESSION_ID: process.env.SESSION_ID || "",
+MONGODB: process.env.MONGODB || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_4265.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
 AUTO_VOICE:"true"
